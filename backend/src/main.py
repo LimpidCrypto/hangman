2144429c1dffeb6609f76ad677bfa363a97fdd3d
LimpidCrypto <konermann@limpidcrypto.com>
2024-11-13 19:32:15 +0000

    add flask server

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,14 @@
+from flask import Flask
+from flask_cors import CORS
+
 
 def main():
-    print("Hello, World!")
+    APP = Flask(__name__)
+    CORS(APP)
+
+    APP.debug = True
+    APP.run(host="0.0.0.0", port=3000)
 
 if __name__ == "__main__":
+    # start Flask server
     main()
