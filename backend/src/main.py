0ae58801c41d7bb209c5bf30f5b6503a9be4e91b
LimpidCrypto <konermann@limpidcrypto.com>
2024-11-28 11:53:50 +0000

    add get_random_word route

--- backend/src/main.py
+++ backend/src/main.py
@@ -10,6 +10,7 @@ def main():
     APP.register_blueprint(WORDS_CONTROLLER)
 
     APP.debug = True
+    # ensure that the JSON response is utf-8 encoded
     APP.config['JSON_AS_ASCII'] = False
     APP.json.ensure_ascii=False
     APP.run(host="0.0.0.0", port=3000)
